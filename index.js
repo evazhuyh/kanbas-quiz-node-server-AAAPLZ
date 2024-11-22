85b0417502f7a25b79feecce013eb5a7a4758272
evazhuyh <zhu.yihua@northeastern.edu>
2024-11-22 15:31:59 -0500

    a5 4.2.1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
+
 const app = express();
+UserRoutes(app);
 app.use(cors());
 app.use(express.json());
 Lab5(app);

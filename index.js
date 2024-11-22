cdba11b0f30f5a0708fc772f261f545bb7101523
evazhuyh <zhu.yihua@northeastern.edu>
2024-11-22 07:04:52 -0500

    a5 3.5.5

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import HelloRoutes from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
 const app = express();
 
-HelloRoutes(app);
+app.use(cors());
 Lab5(app);
 
-app.listen(process.env.PORT || 4000);
+app.listen(4000);

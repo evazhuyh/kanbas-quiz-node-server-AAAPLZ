7950f04590ec21f3779c42c5bd63149c8f1c1aff
evazhuyh <zhu.yihua@northeastern.edu>
2024-11-22 19:02:30 -0500

    a5 4.3.2

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -1,19 +1,55 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 export default function UserRoutes(app) {
   const createUser = (req, res) => { };
   const deleteUser = (req, res) => { };
   const findAllUsers = (req, res) => { };
   const findUserById = (req, res) => { };
-  const updateUser = (req, res) => { };
-  const signup = (req, res) => { };
-  const signin = (req, res) => {
-    const { username, password } = req.body;
-    currentUser = dao.findUserByCredentials(username, password);
+  const updateUser = (req, res) => {
+    const userId = req.params.userId;
+    const userUpdates = req.body;
+    dao.updateUser(userId, userUpdates);
+    const currentUser = dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
-  const signout = (req, res) => { };
-  const profile = (req, res) => { };
+
+  const signup = (req, res) => {
+    const user = dao.findUserByUsername(req.body.username);
+    if (user) {
+      res.status(400).json(
+        { message: "Username already in use" });
+      return;
+    }
+    const currentUser = dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
+    res.json(currentUser);
+  };
+
+  const signin = (req, res) => {
+    const { username, password } = req.body;
+    const currentUser = dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
+  };
+
+  const signout = (req, res) => {
+    req.session.destroy();
+    res.sendStatus(200);
+  };
+
+  const profile = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
+    res.json(currentUser);
+  };
+
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 import express from "express";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import session from "express-session";
+import "dotenv/config";
 import UserRoutes from "./Kanbas/Users/routes.js";
 
 const app = express();
-UserRoutes(app);
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+ ); 
+ const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));  
 app.use(express.json());
+UserRoutes(app);
+//ModuleRoutes(app);
+//CourseRoutes(app);
+//AssignmentRoutes(app);
+
 Lab5(app);
-app.listen(4000);
+const port = process.env.PORT || 4000;
+app.listen(port);

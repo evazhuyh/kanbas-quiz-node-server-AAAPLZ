14abcd2d6cbdb91b36902c8f06edcb0438ea6331
evazhuyh <zhu.yihua@northeastern.edu>
2024-11-22 20:59:17 -0500

    a5 4.4.4

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,5 +1,21 @@
 import * as dao from "./dao.js";
 export default function CourseRoutes(app) {
+  app.put("/api/courses/:courseId", (req, res) => {
+    const { courseId } = req.params;
+    const courseUpdates = req.body;
+    const status = dao.updateCourse(courseId, courseUpdates);
+    res.sendStatus(status);
+  });
+  app.delete("/api/courses/:courseId", (req, res) => {
+    const { courseId } = req.params;
+    dao.deleteCourse(courseId);
+    res.sendStatus(204);
+  });
+  app.post("/api/courses", (req, res) => {
+    const courses = {...req.body, _id: new Date().getTime().toString() };
+    dao.courses.push(course);
+    res.send(courses);
+  });
   app.get("/api/courses", (req, res) => {
     const courses = dao.findAllCourses();
     res.send(courses);

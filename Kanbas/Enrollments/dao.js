dd795887e69ea67cd33f891c59515fae7228695f
evazhuyh <zhu.yihua@northeastern.edu>
2024-12-08 14:30:56 -0500

    a6 5.3.1

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,34 +1,15 @@
-import Database from "../Database/index.js";
-
-export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  enrollments.push({ _id: new Date().getTime().toString(), user: userId, course: courseId });
+import model from "./model.js";
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
 }
-
-export function unenrollUserFromCourse(userId, courseId) {
-  Database.enrollments = Database.enrollments.filter(
-    (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
-  );
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.map((enrollment) => enrollment.user);
 }
-
-export function findAllEnrollments() {
-  return Database.enrollments;
+export function enrollUserInCourse(user, course) {
+ return model.create({ user, course });
 }
-
-export function findEnrollmentsByUser(userId) {
-  return Database.enrollments.filter(
-    (enrollment) => enrollment.user === userId
-  );
-}
-
-export function findEnrollmentsByCourse(courseId) {
-  return Database.enrollments.filter(
-    (enrollment) => enrollment.course === courseId
-  );
-}
-
-export function findEnrollment(userId, courseId) {
-  return Database.enrollments.find(
-    (enrollment) => enrollment.user === userId && enrollment.course === courseId
-  );
+export function unenrollUserFromCourse(user, course) {
+ return model.deleteOne({ user, course });
 }
